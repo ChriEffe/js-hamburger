ec05f430b32ed56b40816b7880bcdab8f02ee47a
ChriEffe <christianferrazzano93@gmail.com>
2021-11-22 15:34:46 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+// Seleziono div hamburger-menu
+const menu = document.querySelector('div:last-child');
+
+// Selezino elemento da attivare
+const show = document.querySelector('.fa-bars');
+
+// Chiudo menù
+const hidden = document.querySelector('.fa-times');
+
+// Collego classe css per visualizzare menù
+show.addEventListener('click', function(){
+    menu.classList.add('active');
+})
+
+// Chiudo visualizzazione menù
+hidden.addEventListener('click', function(){
+    menu.classList.remove('active');
+})
